867e01034c3d7ebdfb00ae6864891ced9ebb5a04
Tuukkaj <juusela.tuukka@gmail.com>
2019-10-08 10:01:38 +0300

    Initial project creation

---
+++ weather_app/weather_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather_app
+//
+//  Created by Tuukka Juusela on 08/10/2019.
+//  Copyright © 2019 Tuukka Juusela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
